cd00b63a98d6e8d5b881bedec166d7d61fcd0010
Aditya Singh <adityaa9170843@gmail.com>
2025-03-09 21:57:59 +0530

    main added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Button from './design/Button'
+import React from "react";
+import Button from "./design/Button";
+import start from "../assets/svg/start.svg";
 function Main() {
   return (
-    <div className='flex min-h-screen w-full bg-[url("./assets/bg.png")] bg-cover bg-no-repeat bg-center '>
+    <div className=" relative  m-c flex  items-center ">
+      <div className="w-[50%] relative ">
+        <div className="flex flex-col items-center gap-6 m-18 w-[100%] h-full  ">
+          <h1 className="lg:whitespace-normal lg:w-[50rem] lg:mb-[4rem] text-center font-semibold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem] bg-gradient-to-t from-pink-500 via-pink-300 to-white bg-clip-text text-transparent uppercase">
+            welcome player1
+          </h1>
+          <h4 className="lg:whitespace-normal lg:w-[30rem]  lg:mb-[4rem] text-[1rem] lg:text-[2rem] leading-normal text-white uppercase">
+            press enter to start the game
+          </h4>
+          <Button className={" lg:mb-[4rem] mt-[2rem]"}>Start</Button>
+          <div>
+            <h1 className="uppercase mt-[10rem] text-white text-2xl">Follow me at</h1>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- src/components/design/Button.jsx
+++ src/components/design/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import buttonSVG from "../../assets/svg/buttonSVG.svg";
-
+import start from "../../assets/svg/start.svg";
 function Button({ className, href, onClick, children }) {
   return (
     <button
-      className={`relative flex items-center p-7 w-40 h-20  justify-center ${className || ""} cursor-pointer`}
+      className={`relative flex items-center p-7 lg:w-60 lg:h-40  justify-center ${className || ""} cursor-pointer`}
       onClick={onClick}
     >
       
@@ -13,7 +13,13 @@ function Button({ className, href, onClick, children }) {
         src={buttonSVG}
         alt="Button Background"
       />
-      <span className="relative z-10 text-white font-bold text-[0.9rem] mb-3 uppercase">{children}</span>
+      <span className="relative bg-gradient-to-t from-pink-500 via-pink-300 to-white bg-clip-text text-transparent text-[1.5rem] mb-1 uppercase">{children}</span>
+      <img 
+        src={start}
+        
+        className="absolute right-[-6rem] lg:right-[-9rem] w-[80px] lg:w-[90px] " 
+        alt="Start Icon" 
+      />
     </button>
   );
 }
