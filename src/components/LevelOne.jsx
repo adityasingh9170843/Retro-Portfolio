8a617a090a183355492590c3ec077a8bbd1aa1a7
Aditya Singh <adityaa9170843@gmail.com>
2025-03-15 22:31:15 +0530

    levelone component edited

--- src/components/LevelOne.jsx
+++ src/components/LevelOne.jsx
@@ -10,7 +10,7 @@ function LevelOne() {
       <h1 className=" text-2xl mb-8 lg:mb-8 sm:text-3xl md:text-4xl lg:text-5xl bg-gradient-to-t from-pink-500 via-pink-300 to-white bg-clip-text text-transparent uppercase animate-neon-flicker ">
         level 1
       </h1>
-      <h2 className="text-white text-2xl mt-6 sm:text-3xl md:text-4xl lg:text-5xl ">know about me</h2>
+      <h2 className="text-white text-3xl mt-6 sm:text-3xl md:text-4xl lg:text-5xl ">know about me</h2>
        
 
       <PixelatedButton showIcon={false} className={"mt-4 lg:mt-12"}>
